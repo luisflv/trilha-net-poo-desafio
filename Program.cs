f69fcc4cabc056a0db26970719fcd17f0c54933f
luisflv <luisflv@gmail.com>
2025-03-18 14:39:56 -0300

    Projeto concluído. Smartphones criados.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
+
+Console.Clear();
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+
+Smartphone nokia = new Nokia("123456", "Modelo 1", "111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone Iphone: ");
+
+
+Smartphone iphone = new Iphone("78910", "Modelo 2", "222222", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
